8c69fea4159e6f5caf77a42f388d0e948660a5f3
rakshith <rakshit.s.gowda@gmail.com>
2021-02-13 09:35:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SearchForm from './SearchForm'
+import Stories from './Stories'
+import Buttons from './Buttons'
+function App() {
+  return <>
+    <SearchForm />
+    <Buttons />
+    <Stories />
+  </>
+}
+
+export default App
